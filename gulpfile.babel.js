c4b92552c97cf4bfc69d75be21c977d29438ef38
Paul Kozlovitch <tnk2005@gmail.com>
2018-08-28 23:49:25 +0200

    WIP added: gitlab CI deployment

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,88 +6,97 @@ import runSequence from 'run-sequence';
 import webpackStream from 'webpack-stream';
 import webpack from 'webpack';
 import {spawn} from 'child_process';
+import chromeWebstoreUpload from 'chrome-webstore-upload';
 
 const $ = gulpLoadPlugins();
+const webStore = chromeWebstoreUpload({
+	extensionId: 'lenehnfieohimgadpmnphhbgikfnoale',
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	refreshToken: process.env.REFRESH_TOKEN,
+});
 
 gulp.task('extras', () => {
-  return gulp.src([
-    'app/*.*',
-    'app/_locales/**',
-    '!app/scripts.babel',
-    '!app/*.json',
-    '!app/*.html',
-  ], {
-    base: 'app',
-    dot: true
-  }).pipe(gulp.dest('dist'));
+	return gulp.src([
+		'app/*.*',
+		'app/_locales/**',
+		'app/styles/**',
+		'app/scripts/**',
+		'!app/scripts.babel',
+		'!app/*.json',
+		'!app/*.html',
+	], {
+		base: 'app',
+		dot: true
+	}).pipe(gulp.dest('dist'));
 });
 
 function lint(files, options) {
-  return () => {
-    return gulp.src(files)
-      .pipe($.eslint(options))
-      .pipe($.eslint.format());
-  };
+	return () => {
+		return gulp.src(files)
+			.pipe($.eslint(options))
+			.pipe($.eslint.format());
+	};
 }
 
 gulp.task('lint', lint('app/scripts.babel/**/*.js', {
-  env: {
-    es6: true
-  },
-  parserOptions: {
-    sourceType: 'module'
-  }
+	env: {
+		es6: true
+	},
+	parserOptions: {
+		sourceType: 'module'
+	}
 }));
 
 gulp.task('images', () => {
-  return gulp.src('app/images/**/*')
-    .pipe($.if($.if.isFile, $.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    }))
-    .on('error', function (err) {
-      console.log(err);
-      this.end();
-    })))
-    .pipe(gulp.dest('dist/images'));
+	return gulp.src('app/images/**/*')
+		.pipe($.if($.if.isFile, $.cache($.imagemin({
+			progressive: true,
+			interlaced: true,
+			// don't remove IDs from SVGs, they are often used
+			// as hooks for embedding and styling
+			svgoPlugins: [{cleanupIDs: false}]
+		}))
+		.on('error', function (err) {
+			console.error(err);
+			this.end();
+		})))
+		.pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('html',  () => {
-  return gulp.src('app/*.html')
-    .pipe($.htmlmin({
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: true,
-      removeComments: true
-    }))
-    .pipe(gulp.dest('dist'));
+	return gulp.src('app/*.html')
+		.pipe($.htmlmin({
+			collapseWhitespace: true,
+			minifyCSS: true,
+			minifyJS: true,
+			removeComments: true
+		}))
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('chromeManifest', () => {
-  return gulp.src('app/manifest.json')
-    .pipe($.chromeManifest({
-      buildnumber: true,
-      background: {
-        target: 'scripts/background.js',
-        exclude: [
-          'scripts/chromereload.js'
-        ]
-      }
-    }))
-    .pipe(gulp.dest('dist'));
+	return gulp.src('app/manifest.json')
+		.pipe($.chromeManifest({
+			buildnumber: true,
+			background: {
+				target: 'scripts/background.js',
+				exclude: [
+					'scripts/chromereload.js'
+				]
+			}
+		}))
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('babel', () => {
-  return gulp.src('app/scripts.babel')
-    .pipe(webpackStream(require('./webpack.config.js'), webpack)
-      .on('error', function (err) {
-        console.log(err);
-        this.emit('end');
-      }))
-    .pipe(gulp.dest('app/scripts/'))
+	return gulp.src('app/scripts.babel')
+		.pipe(webpackStream(require('./webpack.config.js'), webpack)
+			.on('error', function (err) {
+				console.error(err);
+				this.emit('end');
+			}))
+		.pipe(gulp.dest('app/scripts/'))
 });
 
 gulp.task('clean', del.bind(null, ['dist']));
@@ -97,38 +106,58 @@ gulp.task('vue', () => {
 })
 
 gulp.task('watch', ['lint', 'babel', 'vue'], () => {
-  $.livereload.listen();
+	$.livereload.listen();
 
-  gulp.watch([
-    'app/*.html',
-    'app/scripts/**/*.js',
-    'app/scripts/**/*.vue',
-    'app/images/**/*',
-    'app/styles/**/*',
-    'app/_locales/**/*.json'
-  ]).on('change', $.livereload.reload);
+	gulp.watch([
+		'app/*.html',
+		'app/scripts/**/*.js',
+		'app/scripts/**/*.vue',
+		'app/images/**/*',
+		'app/styles/**/*',
+		'app/_locales/**/*.json'
+	]).on('change', $.livereload.reload);
 
-  gulp.watch(['app/scripts.babel/**/*.js', 'app/scripts.babel/**/*.vue'], ['lint', 'babel']);
+	gulp.watch(['app/scripts.babel/**/*.js', 'app/scripts.babel/**/*.vue'], ['lint', 'babel']);
 });
 
 gulp.task('size', () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+	return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
 gulp.task('package', function () {
-  var manifest = require('./dist/manifest.json');
-  return gulp.src('dist/**')
-      .pipe($.zip('chrome ext-' + manifest.version + '.zip'))
-      .pipe(gulp.dest('package'));
+	var manifest = require('./dist/manifest.json');
+	return gulp.src('dist/**')
+			.pipe($.zip('chrome ext-' + manifest.version + '.zip'))
+			.pipe(gulp.dest('package'));
 });
 
 gulp.task('build', (cb) => {
-  runSequence(
-    'lint', 'babel', 'chromeManifest',
-    ['html', 'images', 'extras'],
-    'size', cb);
+	runSequence(
+		'lint', 'babel', 'chromeManifest',
+		['html', 'images', 'extras'],
+		'size', cb);
+});
+
+gulp.task('upload', () => {
+	var manifest = require('./dist/manifest.json');
+	webStore.fetchToken().then(token => {
+		const myZipFile = fs.createReadStream('package/chrome ext-' + manifest.version + '.zip');
+		webStore.uploadExisting(myZipFile, token).then(res => {
+				if (res.uploadState !== 'SUCCESS') {
+					console.error(res.itemError)
+				} else {
+					webStore.publish('default', token).then(res => {
+						console.log(res.status);
+					});
+				}
+		});
+	});
 });
 
 gulp.task('default', ['clean'], cb => {
-  runSequence('build', cb);
+	runSequence('build', cb);
+});
+
+gulp.task('release', ['package'], cb => {
+	runSequence('build', 'package', 'upload', cb);
 });
